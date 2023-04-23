1bb3c699b9a50bea50d9866b6d1bdea3b1a917be
Joseph <azurerayus@gmail.com>
2023-04-23 22:13:29 +0800

    Update xanmod.sh

--- xanmod.sh
+++ xanmod.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+
 checkurl() {
   local url=$1
   if [ -z "$url" ]; then
     echo -e "${Error} 链接为空，请检查网络或稍后再试" && exit 1
   fi
-  status_code=$(curl --write-out %{http_code} --silent --output /dev/null $url)
+  status_code=$(curl --write-out %{http_code} --silent --output /dev/null --location $url)
   if [ $status_code -eq 200 ]; then
     echo -e "连接正常"
   else
@@ -56,19 +57,30 @@ install_xanmod_kernel() {
   # 下载并安装Xanmod内核
   echo -e "开始安装Xanmod内核（指令集：${cpu_instruction_set}）"
   all_kernels=$(curl -s https://api.github.com/repos/xanmod/linux/releases | jq '.[] | select(.prerelease==false) | .tag_name' | tr -d '\"' | grep -v "rt")
+  echo $all_kernels
   latest_kernel="0"
-  
+
   for kernel in $all_kernels; do
-    if [[ $(printf '%s\n' "$kernel" "$latest_kernel" | sort -V | head -n1) != "$latest_kernel" ]]; then
-      latest_kernel=$kernel
+    kernel_number=$(echo $kernel | cut -d '-' -f1)
+    latest_kernel_number=$(echo $latest_kernel | cut -d '-' -f1)
+
+    echo "Comparing kernel_number: $kernel_number, latest_kernel_number: $latest_kernel_number"
+    if [[ $(printf '%s\n' "$kernel_number" "$latest_kernel_number" | sort -V | tail -n1) == "$kernel_number" ]]; then
+        latest_kernel=$kernel
+        echo "Updated latest_kernel to: $latest_kernel"
     fi
 done
 
 
   echo -e "获取到的最新内核版本为：${latest_kernel}"
+  headers_deb_response=$(curl -s https://api.github.com/repos/xanmod/linux/releases/tags/$latest_kernel)
+  echo "Headers response: $headers_deb_response"
+  headers_deb_url=$(echo "$headers_deb_response" | grep "browser_download_url" | grep "amd64.deb" | grep "headers" | grep "${cpu_instruction_set}" | head -n 1 | cut -d\" -f4)
+  
+  kernel_deb_response=$(curl -s https://api.github.com/repos/xanmod/linux/releases/tags/$latest_kernel)
+  echo "Kernel response: $kernel_deb_response"
+  kernel_deb_url=$(echo "$kernel_deb_response" | grep "browser_download_url" | grep "amd64.deb" | grep "image" | grep "${cpu_instruction_set}" | head -n 1 | cut -d\" -f4)
 
-  headers_deb_url=$(curl -s https://api.github.com/repos/xanmod/linux/releases/tags/$latest_kernel | grep "browser_download_url" | grep "amd64.deb" | grep "headers" | grep "${cpu_instruction_set}" | head -n 1 | cut -d\" -f4)
-  kernel_deb_url=$(curl -s https://api.github.com/repos/xanmod/linux/releases/tags/$latest_kernel | grep "browser_download_url" | grep "amd64.deb" | grep "image" | grep "${cpu_instruction_set}" | head -n 1 | cut -d\" -f4)
 
   if [ -z "$headers_deb_url" ] || [ -z "$kernel_deb_url" ]; then
     echo -e "${Error} 未找到headers或内核下载链接，请检查网络或稍后再试" && exit 1
@@ -82,22 +94,15 @@ done
   wget -N -O xanmod-headers.deb $headers_deb_url
   wget -N -O xanmod-kernel.deb $kernel_deb_url
   
-  if [[ "${release}" == "supported" ]]; then
   sudo dpkg -i xanmod-headers.deb
   sudo dpkg -i xanmod-kernel.deb
   sudo apt-get install -f
-  else
-  echo -e "${Error} 不支持此系统，仅支持Debian和Ubuntu !" && exit 1
-  fi
   
-  current_kernel=$(uname -r)
-  echo -e "当前系统内核版本为：${current_kernel}"
-  
-  if [[ "${current_kernel}" == "${latest_kernel}" ]]; then
+  echo "Checking /boot directory for vmlinuz files:"
+  ls /boot | grep "vmlinuz"
+  installed_kernel=$(find /boot -name "vmlinuz-*-xanmod*" | sort -V | tail -n 1 | sed -e "s/\/boot\/vmlinuz-//")
+  echo -e "已成功安装新内核：${installed_kernel}"
   echo -e "Xanmod内核安装完成，请重启系统以启用新内核"
-  else
-  echo -e "${Error} 内核安装失败，请检查日志并尝试重新安装"
-  fi
 }
 
 install_xanmod_kernel
